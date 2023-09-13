0a3d8c0009fc5caa4392725bab8066853fe8b306
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-14 02:24:29 +0600

    adding files

---
+++ dash-board-demo.php
@@ -0,0 +1,11 @@
+<?php 
+/*
+ * Plugin Name:       Dash Board Widget
+ * Description:       Handle the basics with this plugin.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Rezwanul Haque
+ * Author URI:        https://author.example.com/
+ * Text Domain:      dash-board-widget
+ */
