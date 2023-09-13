8974b66435a5d69968f4eb08fadaadb340efaab5
Rezwanul Haque <74784389+REZWANUL1@users.noreply.github.com>
2023-09-14 04:12:54 +0600

    adding configure files

--- dash-board-demo.php
+++ dash-board-demo.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*
  * Plugin Name:       Dash Board Widget
  * Description:       Handle the basics with this plugin.
@@ -9,3 +9,50 @@
  * Author URI:        https://author.example.com/
  * Text Domain:      dash-board-widget
  */
+if (!defined('ABSPATH')) {
+   exit;
+}
+
+//? adding widget
+add_action('wp_dashboard_setup', 'add_demo_widget');
+function add_demo_widget()
+{
+   if (current_user_can('edit_dashboard')) {
+      wp_add_dashboard_widget('rez_demo_widget', 'Dashboard Widget Demo By Rez', 'rez_demo_widget_output_callback', 'rez_demo_widget_configure_callback');
+   } else {
+      wp_add_dashboard_widget('rez_demo_widget', 'Dashboard Widget Demo By Rez', 'rez_demo_widget_output_callback');
+   }
+}
+
+//? adding callback
+function rez_demo_widget_output_callback()
+{
+   $number_of_posts = get_option('rez_demo_widget_nop', 5);
+   $feeds = array(
+      [
+         'url' => 'http://localhost/addon_elementor/feed/',
+         'items' =>  $number_of_posts,
+         'show_summary' => 0,
+         'show_author' => 0,
+         'show_date' => 1
+      ]
+   );
+   wp_dashboard_primary_output('rez_demo_widget', $feeds);
+}
+
+//? adding configure
+function rez_demo_widget_configure_callback()
+{
+   $number_of_posts = get_option('rez_demo_widget_nop', 5);
+   if (isset($_POST['dashboard-widget-nonce']) && wp_verify_nonce($_POST['dashboard-widget-nonce'], 'edit-dashboard-widget_rez_demo_widget')) {
+      if (isset($_POST['rez_dbw_name']) && ($_POST['rez_dbw_name']) > 0) {
+         $number_of_posts = sanitize_text_field($_POST['rez_dbw_name']);
+         update_option("rez_demo_widget_nop", $number_of_posts);
+      }
+   }
+
+?>
+   <label>Number of Posts</label>
+   <input type="text" name="rez_dbw_name" class="widefat" id="rez_dbw_name" value="<?php esc_attr_e($number_of_posts); ?>">
+<?php
+}
